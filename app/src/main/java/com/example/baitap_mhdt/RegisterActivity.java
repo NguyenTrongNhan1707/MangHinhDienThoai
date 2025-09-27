e71e30846c04fa1f8dcdef6c668a3a1141ec0941
NguyenTrongNhan1707 <nguyentrongnhan1707@gmail.com>
2025-09-27 16:40:55 +0700

    Thay doi hai mang hinh qua lai

--- app/src/main/java/com/example/baitap_mhdt/RegisterActivity.java
+++ app/src/main/java/com/example/baitap_mhdt/RegisterActivity.java
@@ -1,6 +1,9 @@
 package com.example.baitap_mhdt;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.baitap_mhdt.R;
+
 public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        TextView btn=findViewById(R.id.alreadyHaveAccount);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View w){
+                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+            }
         });
     }
 }
